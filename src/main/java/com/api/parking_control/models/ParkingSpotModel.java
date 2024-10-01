aefd81075172dcb20deff713d918a1c0926ac4f7
Alti1302 <altieres.silva@hotmail.com>
2024-09-30 23:51:55 -0300

    Fixed - Corrigido dependência Jpa e alterado os imports das anoteÃações para Jakarta

---
+++ src/main/java/com/api/parking_control/models/ParkingSpotModel.java
@@ -0,0 +1,10 @@
+package com.api.parking_control.models;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import java.io.Serializable;
+
+@Entity
+@Table  (name = "TB_PARKING_SPOT")
+public class ParkingSpotModel implements Serializable {
+    private static final long serialVersionUID=1L;
+}
